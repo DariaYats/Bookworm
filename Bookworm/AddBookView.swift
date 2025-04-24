40f35f9916957cc6e7cda55565741227bdb3a36d
Daria Yats <dariayatsyniuk@gmail.com>
2025-04-24 14:05:23 +0200

    create book model

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Дарья Яцынюк on 24.04.2025.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
